68d3de211be6fcd6be2feafc5cc29045efca7b2f
Matthew Butler <butler.matthew@gmail.com>
2016-09-07 17:17:03 -0300

    Clear main until ready.
    Prevents warnings/errors

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import "fmt"
-
-import "github.com/butlermatt/dsdoc/docs"
-
 func main() {
-	fmt.Println(trim.LinkDoc)
-	fmt.Println(trim.NodeDoc)
-	fmt.Println(trim.ActionDoc)
+
 }
